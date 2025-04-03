2b7203b0e53619415a3bfd768df4b6c65f7bbe64
Wai Chan <wcc325@nyu.edu>
2025-04-03 17:12:34 -0400

    added visual queue to the dom and working rock paper scissors

--- index.js
+++ index.js
@@ -2,53 +2,54 @@ const getComputerChoice = () => {
     return Math.floor(Math.random() * 3) + 1
 }
 
-const getHumanChoices = () => {
-    const input = prompt("rock, paper, or scissors player please choose?").toLowerCase()
-    const humanNum = input == "rock" ? 1 : 
-                     input == "paper" ? 2:
-                     input == "scissors" ? 3:
-                     null;
-    return humanNum
-
-}
-
 let humanScore = 0
-
 let computerScore = 0
 
+const resetScore = () => {
+    humanScore = 0
+    computerScore = 0
+}
+
+const convertHumanChoicesToNum = (input) => {
+    const output = input.toLowerCase()
+    const humanNum = output == "rock" ? 1 : 
+                     output == "paper" ? 2:
+                     output == "scissors" ? 3:
+                     null;
+    return humanNum
+}
+
 const playRound = (input) => {
-    if (input == "round"){
-        const computerChoice = getComputerChoice()
-        const playerChoice = getHumanChoices()
-        console.log(computerChoice)
-        if (computerChoice == getHumanChoices){
-            console.log("It's a draw")
-        }
-        else if (computerChoice == 1 && playerChoice == 2){
-            console.log("Player picked paper and computer picked rock. Player has won.")
-            humanScore += 1
-        }
-        else if (computerChoice == 1 && playerChoice == 3){
-            console.log("Player picked scissors and computer picked rock. Computer has won.")
-            computerScore += 1
-        }
-        else if (computerChoice == 2 && playerChoice == 1){
-            console.log("Player picked rock and computer picked paper. Computer has won.")
-            computerScore += 1
-        }
-        else if (computerChoice == 2 && playerChoice == 3){
-            console.log("Player picked scissors and computer picked paper. Player has won.")
-            humanScore += 1
-        }
-        else if (computerChoice == 3 && playerChoice == 1){
-            console.log("Player picked rock and computer picked scissors. Player has won.")
-            humanScore += 1
-        }
-        else if (computerChoice == 3 && playerChoice == 2){
-            console.log("Player picked paper and computer picked scissors. Computer has won.")
-            computerScore += 1
-        }
-    } 
+    const playerChoice = convertHumanChoicesToNum(input);
+    const computerChoice = getComputerChoice();
+    const result = document.getElementById('result')
+    if (computerChoice == playerChoice){
+        result.textContent = "It's a draw"
+    }
+    else if (computerChoice == 1 && playerChoice == 2){
+        result.textContent = "Player picked paper and computer picked rock. Player has won."
+        humanScore += 1
+    }
+    else if (computerChoice == 1 && playerChoice == 3){
+        result.textContent = "Player picked scissors and computer picked rock. Computer has won."
+        computerScore += 1
+    }
+    else if (computerChoice == 2 && playerChoice == 1){
+        result.textContent = "Player picked rock and computer picked paper. Computer has won."
+        computerScore += 1
+    }
+    else if (computerChoice == 2 && playerChoice == 3){
+        result.textContent = "Player picked scissors and computer picked paper. Player has won."
+        humanScore += 1
+    }
+    else if (computerChoice == 3 && playerChoice == 1){
+        result.textContent = "Player picked rock and computer picked scissors. Player has won."
+        humanScore += 1
+    }
+    else if (computerChoice == 3 && playerChoice == 2){
+        result.textContent = "Player picked paper and computer picked scissors. Computer has won."
+        computerScore += 1
+    }
 }
 
 const getActivity = () => {
@@ -58,4 +59,36 @@ const getActivity = () => {
     console.log("human score: " + humanScore)
 }
 
-getActivity()
+const playFiveRounds = () => {
+    const container = document.getElementById('container');
+    const winner = document.getElementById('winner');
+    const player = document.getElementById('player')
+    const computer = document.getElementById('computer')
+
+        for (let button of container.children){
+            button.addEventListener("click", () => {
+                winner.textContent = ""
+                const value = button.textContent;
+                playRound(value)
+                player.textContent = "Player Score: " + humanScore;
+                computer.textContent = "Computer Score: " + computerScore;
+                if (computerScore === 5){
+                    winner.textContent = "Computer has Won"
+                    resetScore()
+                }
+                else if (humanScore === 5){
+                    winner.textContent = "Player has Won"
+                    resetScore()
+                }
+            })
+        }
+
+    
+}
+
+// getActivity()
+
+document.addEventListener("DOMContentLoaded", function() {
+    playFiveRounds();    
+})
+
